b44ef1392c37721dbc879bc98aa2dcb27538e9e0
Smurkley <37908288+Smurkley@users.noreply.github.com>
2018-04-03 11:05:48 -0500

    Lab 12
    Lab 12 files

---
+++ Lab12/Lab12/Defalut.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Defalut.aspx.cs" Inherits="Lab12.Defalut" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+    
+                <asp:TextBox ID="txtSearch" runat="server"></asp:TextBox>
+                <br />
+                <asp:Button ID="btnSearch" runat="server" OnClick="Search_Click" Text="Search" />
+                <br />
+                <asp:Label ID="lblError" runat="server" Text="Error"></asp:Label>
+    
+                <asp:GridView runat="server" ID="GridView1" OnSelectedIndexChanged="GridView1_SelectedIndexChanged"></asp:GridView>
+                <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Lab12/Lab12/Defalut.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab12
+{
+    public partial class Defalut : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtSearch.Text))
+            {
+                lblError.Text = ("please enter valid search keyword!");
+
+            }
+            lblError.Text = String.Empty;
+
+            var books = Books.GetBooks();
+
+            var sortedbooks = from bookItem in books
+                              where bookItem.Title.Contains("txtSearch.Text") 
+                              orderby bookItem.Title
+                              select bookItem;
+
+            GridView1.DataSource = sortedbooks;
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Search_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
