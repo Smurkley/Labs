a720a071c7635cf697ef43d39d2b3eed85ef5344
Smurkley <37908288+Smurkley@users.noreply.github.com>
2018-04-05 11:09:17 -0500

    Lab 13
    Day 13 lab 13 upload

---
+++ Day 14 - Activities-and-Intents/Day 14 - Activities-and-Intents/Resources/GroceryList/GroceryList/DetailsActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+
+namespace GroceryList
+{
+	[Activity(Label = "Details")]			
+	public class DetailsActivity : Activity
+	{
+        txtView txtName;
+        txtView txtCount;
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+			SetContentView(Resource.Layout.Details);
+
+			int position = 0;
+
+            // TODO
+            position = Intent.GetIntExtra("ItemPosition", -1);
+
+            var item = MainActivity.Items[position];
+
+            txtCount = FindViewById<TextView>(Resource.Id.countTextView);
+            txtName = FindViewById<TextView>(Resource.Id.nameTextView);
+
+            txtName.Text = "Name: "  + item.Name;
+			txtCount.Text = "Count: " + item.Count.ToString();
+		}
+	}
+}
