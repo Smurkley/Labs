fb6a11cce345669f8e060b826a34c6b7ffe18bd8
Smurkley <37908288+Smurkley@users.noreply.github.com>
2018-04-12 11:16:41 -0500

    Add files via upload

---
+++ Lab15/Lab15/Activities/MainActivity.cs
@@ -0,0 +1,50 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using Lab15.Activities;
+
+
+namespace Lab15
+{
+    [Activity(Label = "Book Inventory", MainLauncher = true)]
+    public class MainActivity : TabActivity
+    {
+        EditText txtEmail;
+        EditText txtPassword;
+        EditText btnLogin;
+        EditText btnRegister;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            txtEmail = FindViewById<EditText>(Resource.Id.txtEmail);
+            txtPassword = FindViewById<EditText>(Resource.Id.txtEmail);
+            btnLogin = FindViewById<Button>(Resource.Id.btnLogin);
+            btnRegister = FindViewById<Button>(Resource.Id.btnRegister);
+
+            btnLogin.Click += BtnLogin_Click;
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            btnRegister.Click += BtnRegister_Click;
+            btnRegister.Click += BtnRegister_Click1;
+        }
+
+        private void BtnRegister_Click1(object sender, EventArgs e)
+        {
+            StartActivity(typeof(RegisterActivity));
+        }
+
+        private void BtnRegister_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+
+}
+
---
+++ Lab15/Lab15/Activities/RegisterActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+
+namespace Lab15.Activities
+{
+    [Activity(Label = "RegisterActivity")]
+    public class RegisterActivity : Activity
+    {
+        EditText txtEmail;
+        EditText txtPassword;
+        EditText txtConfirmPassword;
+        EditText btnRegister;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+
+            SetContentView(Resource.Layout.Register);
+
+            txtEmail = FindViewById<EditText>(Resource.Id.txtEmail);
+            txtPassword = FindViewById<EditText>(Resource.Id.txtEmail);
+            txtConfirmPassword = FindViewById<EditText>(Resource.Id.btnLogin);
+            btnRegister = FindViewById<Button>(Resource.Id.btnRegister);
+        }
+    }
+}
